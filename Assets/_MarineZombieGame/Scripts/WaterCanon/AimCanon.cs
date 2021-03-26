a910ebd6f3e2f3f08b062915c465a8906c4c253d
ImAShark <killersshadowlol@gamil.com>
2021-03-26 15:54:16 +0100

    start watercanon

---
+++ Assets/_MarineZombieGame/Scripts/WaterCanon/AimCanon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimCanon : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 mouse = Input.mousePosition;
+        Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(new Vector3(
+                                                            mouse.x,
+                                                            mouse.y,
+                                                            transform.position.y));
+        Vector3 forward = mouseWorld - transform.position;
+        transform.rotation = Quaternion.LookRotation(new Vector3(forward.x, 0, forward.z), Vector3.up);
+    }
+}
---
+++ Assets/_MarineZombieGame/Scripts/WaterCanon/WaterPressure.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterPressure : MonoBehaviour
+{
+    [SerializeField] private float maxCapacity = 100;
+    [SerializeField] private float energyCost;
+    [SerializeField] private float rechargeRate;
+    
+    [SerializeField] private GameObject beam;
+
+    private float pressure;
+    private bool isCharging = false;
+
+    void Start()
+    {
+        pressure = maxCapacity;
+        beam.SetActive(false);
+    }
+
+    void Update()
+    {
+        Shoot();
+    }
+
+    private void Shoot()
+    {
+        if (Input.GetKey(KeyCode.Mouse0) && pressure > 0 && !isCharging)
+        {
+            beam.SetActive(true);
+            pressure = pressure - energyCost * Time.deltaTime;
+        }
+        else
+        {
+            beam.SetActive(false);
+        }
+
+        if (pressure >= maxCapacity)
+        {
+            pressure = maxCapacity;
+        }
+        else if(!Input.GetKey(KeyCode.Mouse0))
+        {
+            pressure += rechargeRate * Time.deltaTime;
+        }
+
+        if (pressure < 0 && beam.activeSelf)
+        {
+            isCharging = true;
+        }
+        else if (pressure >= 5)
+        {
+            isCharging = false;
+        }
+    }
+}
