762d2ff1dace1dac1b1bc4223de4911ed66677e1
max <1223max@gmail.com>
2021-03-26 13:34:55 +0100

    skeleton script of the wave spawner
    skelton script made for the wave spawner so we can work on it when the nesesary scripts have been made

---
+++ Assets/scripts/WaveSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class WaveSystem : MonoBehaviour
+{
+    private int waveNumber;
+
+    public List<Transform> spawnPoints = new List<Transform>();
+    
+    void SendWave()
+    {
+
+    }
+}
