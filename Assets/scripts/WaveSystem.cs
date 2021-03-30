2d639b2445905d319bd6d94732ded0cbb6d48cea
max <1223max@gmail.com>
2021-03-30 14:47:56 +0200

    the system will now spawn waves of enemy instead of one
    2 for loops have been added so the wavesystem will spawn survivers and zombies per wave and the number of then will incries per wave complieted.
    the way the wave number will be kept track of and when the new wave will start will be discused further

--- Assets/scripts/WaveSystem.cs
+++ Assets/scripts/WaveSystem.cs
@@ -11,6 +11,14 @@ public class WaveSystem : MonoBehaviour
     
     public void SendWave()
     {
-        Zombie.Instantiate(spawnPoints[Random.Range(0, spawnPoints.Count)]);
+        for (int i = 0; i < (5 * waveNumber); i++)
+        {
+            Survivor.Instantiate(spawnPoints[Random.Range(0, spawnPoints.Count)]);
+        }
+        
+        for (int i = 0; i < (20 * waveNumber); i++)
+        {
+            Zombie.Instantiate(spawnPoints[Random.Range(0, spawnPoints.Count)]);
+        }
     }
 }
