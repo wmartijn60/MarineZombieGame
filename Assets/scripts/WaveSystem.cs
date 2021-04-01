09e6fc8419075432832ee72edea7935557bcbf48
ColinvD <vanDongen.colin@gmail.com>
2021-04-01 12:34:19 +0200

    Zombie prefab updated with animation

--- Assets/scripts/WaveSystem.cs
+++ Assets/scripts/WaveSystem.cs
@@ -7,8 +7,8 @@ public class WaveSystem : MonoBehaviour
     private int waveNumber = 0;
     [SerializeField] private List<GameObject> zombies;
     [SerializeField] private List<GameObject> survivors;
-
-    public List<Transform> spawnPoints = new List<Transform>();
+    [SerializeField] private List<Transform> spawnPoints = new List<Transform>();
+    [SerializeField] private Transform enemies;
 
     private void Update() {
         if (Input.GetKeyDown(KeyCode.J)) {
@@ -24,15 +24,13 @@ public class WaveSystem : MonoBehaviour
             Transform chosenSpawnPoint = spawnPoints[Random.Range(0, spawnPoints.Count)];
             GameObject newSurvivor = GameObject.Instantiate(survivors[Random.Range(0, survivors.Count)], chosenSpawnPoint.position, chosenSpawnPoint.rotation);
             newSurvivor.GetComponent<Survivor>().target = GameObject.FindGameObjectWithTag("Player").transform;
-            //Survivor.Instantiate(spawnPoints[Random.Range(0, spawnPoints.Count)]);
             yield return new WaitForSeconds(0.1f);
         }
         
         for (int i = 0; i < (20 * waveNumber); i++) {
             Transform chosenSpawnPoint = spawnPoints[Random.Range(0, spawnPoints.Count)];
-            GameObject newZombie = GameObject.Instantiate(zombies[Random.Range(0, zombies.Count)], chosenSpawnPoint.position, chosenSpawnPoint.rotation);
+            GameObject newZombie = GameObject.Instantiate(zombies[Random.Range(0, zombies.Count)], chosenSpawnPoint.position, chosenSpawnPoint.rotation, enemies);
             newZombie.GetComponent<Zombie>().target = GameObject.FindGameObjectWithTag("Player").transform;
-            //Zombie.Instantiate(spawnPoints[Random.Range(0, spawnPoints.Count)]);
             yield return new WaitForSeconds(0.1f);
         }
     }
