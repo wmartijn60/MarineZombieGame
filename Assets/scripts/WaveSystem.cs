aa9116cd864d7f07e88f30a70f9d4495b215896b
ColinvD <vanDongen.colin@gmail.com>
2021-04-01 10:50:22 +0200

    added delayed spawning

--- Assets/scripts/WaveSystem.cs
+++ Assets/scripts/WaveSystem.cs
@@ -2,23 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class WaveSystem : MonoBehaviour
 {
-    private int waveNumber;
+    private int waveNumber = 0;
+    [SerializeField] private List<GameObject> zombies;
+    [SerializeField] private List<GameObject> survivors;
 
     public List<Transform> spawnPoints = new List<Transform>();
-    
-    public void SendWave()
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.J)) {
+            StartCoroutine("SendWave");
+        }
+    }
+
+    public IEnumerator SendWave()
     {
+        waveNumber++;
         for (int i = 0; i < (5 * waveNumber); i++)
         {
-            Survivor.Instantiate(spawnPoints[Random.Range(0, spawnPoints.Count)]);
+            Transform chosenSpawnPoint = spawnPoints[Random.Range(0, spawnPoints.Count)];
+            GameObject newSurvivor = GameObject.Instantiate(survivors[Random.Range(0, survivors.Count)], chosenSpawnPoint.position, chosenSpawnPoint.rotation);
+            newSurvivor.GetComponent<Survivor>().target = GameObject.FindGameObjectWithTag("Player").transform;
+            //Survivor.Instantiate(spawnPoints[Random.Range(0, spawnPoints.Count)]);
+            yield return new WaitForSeconds(0.1f);
         }
         
-        for (int i = 0; i < (20 * waveNumber); i++)
-        {
-            Zombie.Instantiate(spawnPoints[Random.Range(0, spawnPoints.Count)]);
+        for (int i = 0; i < (20 * waveNumber); i++) {
+            Transform chosenSpawnPoint = spawnPoints[Random.Range(0, spawnPoints.Count)];
+            GameObject newZombie = GameObject.Instantiate(zombies[Random.Range(0, zombies.Count)], chosenSpawnPoint.position, chosenSpawnPoint.rotation);
+            newZombie.GetComponent<Zombie>().target = GameObject.FindGameObjectWithTag("Player").transform;
+            //Zombie.Instantiate(spawnPoints[Random.Range(0, spawnPoints.Count)]);
+            yield return new WaitForSeconds(0.1f);
         }
     }
 }
