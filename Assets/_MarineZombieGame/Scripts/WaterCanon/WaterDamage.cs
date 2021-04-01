34402a8d0c4cb339ba6e4f777733a1283728763c
Joran de Lang <j.delang@azerion.com>
2021-04-01 10:04:30 +0200

    added particle hit detection

---
+++ Assets/_MarineZombieGame/Scripts/WaterCanon/WaterDamage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterDamage : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem beam;
+
+    private void OnParticleCollision(GameObject other)
+    {
+        Debug.Log(other.name);
+    }
+}
