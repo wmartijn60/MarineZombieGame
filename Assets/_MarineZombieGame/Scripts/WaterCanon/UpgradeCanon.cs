ce0d78504d450fc4d3cf5c2c62cdcf9f57cc0749
ImAShark <killersshadowlol@gmail.com>
2021-04-13 12:12:54 +0200

    coins gotten from survivors can buy upgrades

--- Assets/_MarineZombieGame/Scripts/WaterCanon/UpgradeCanon.cs
+++ Assets/_MarineZombieGame/Scripts/WaterCanon/UpgradeCanon.cs
@@ -13,9 +13,6 @@ public class UpgradeCanon : MonoBehaviour
     [SerializeField] private List<int> upgradeRepressure;
     [SerializeField] private List<int> upgradeLevel;
 
-    public int coins; //for testing
-    
-
     void Start()
     {
         
@@ -24,9 +21,9 @@ public class UpgradeCanon : MonoBehaviour
 
     public void UpgradeCapacity()
     {
-        if (coins > upgradeCost[0] && maxLevel != upgradeLevel[0])
+        if (GameManager.Coins >= upgradeCost[0] && maxLevel != upgradeLevel[0])
         {
-            coins -= upgradeCost[0];
+            GameManager.ChangeCoinAmount(-upgradeCost[0]);
             waterPressure.IncreaseCapacity(upgradeCapacity[upgradeLevel[0]]);
             upgradeLevel[0] += 1;            
         }
@@ -34,9 +31,9 @@ public class UpgradeCanon : MonoBehaviour
 
     public void UpgradeRecharge()
     {
-        if (coins > upgradeCost[1] && maxLevel != upgradeLevel[1])
+        if (GameManager.Coins >= upgradeCost[1] && maxLevel != upgradeLevel[1])
         {
-            coins -= upgradeCost[1];
+            GameManager.ChangeCoinAmount(-upgradeCost[1]);
             waterPressure.IncreaseRechargeRate(upgradeRecharge[upgradeLevel[1]]);
             upgradeLevel[1] += 1;
         }
@@ -44,9 +41,9 @@ public class UpgradeCanon : MonoBehaviour
 
     public void UpgradeRepressure()
     {
-        if (coins > upgradeCost[2] && maxLevel != upgradeLevel[2])
+        if (GameManager.Coins >= upgradeCost[2] && maxLevel != upgradeLevel[2])
         {
-            coins -= upgradeCost[2];
+            GameManager.ChangeCoinAmount(-upgradeCost[2]);
             waterPressure.SetRepressureValue(upgradeRepressure[upgradeLevel[2]]);
             upgradeLevel[2] += 1;
         }
