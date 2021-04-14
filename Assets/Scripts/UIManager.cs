0101f3f590b494b795927a5fbfee945726a50a51
ColinvD <vanDongen.colin@gmail.com>
2021-04-14 16:36:38 +0200

    Revert "ScriptsFolderRemoval"
    This reverts commit ed598cc1c4d08adf7066074f35f825dde41886a8.

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private Text coinsText;
+    [SerializeField] private GameObject restPanel;
+    [SerializeField] private Text countDownText;
+
+    public void UpdateUI() {
+        coinsText.text = GameManager.Coins + " coins";
+    }
+
+    // Name may need to get changed to a better one
+    public void CanvasSwitch() {
+        restPanel.SetActive(!restPanel.activeSelf);
+    }
+
+    public void UpdateCountDownText(int currentSeconds) {
+        countDownText.text = "Next wave in: " + currentSeconds + " seconds";
+    }
+}
