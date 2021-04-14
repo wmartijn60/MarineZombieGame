c2d3f92a9c770d6d9a1422b1b3f93fa62a05a80b
ColinvD <vanDongen.colin@gmail.com>
2021-04-14 16:05:10 +0200

    Revert "Removed unknown scripts folder"
    This reverts commit 3880794f91275171c116a84c901cd0ef6a122a95.

---
+++ Assets/scripts/CoinGiver.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CoinGiver : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Survivor")
+        {
+            GameManager.ChangeCoinAmount(collision.GetComponent<Survivor>().CoinsAmountToGive);
+            // activate coin animation survivor
+        }
+        if (collision.tag == "Survivor" || collision.tag == "Zombie")
+        {
+            // this may have to be changed to a better / proper way of doing it
+            Destroy(collision.gameObject, 3f);
+        }
+    }
+}
---
+++ Assets/scripts/TargetMover.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class TargetMover : MonoBehaviour
+{
+    private Vector3 targetStartPosition;
+    private Vector3 moveDirection;
+    private GameObject target;
+    [SerializeField] private float maximumDistanceAway = 1;
+    [SerializeField] private float stepSize = 0.05f;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+        targetStartPosition = target.transform.position;
+        moveDirection = Vector3.right * stepSize;
+    }
+
+    void Update()
+    {
+        MoveTarget();
+    }
+
+    private void MoveTarget() {
+        if (Vector3.Distance(targetStartPosition, target.transform.position) >= maximumDistanceAway) {
+            moveDirection *= -1;
+        }
+        target.transform.position += moveDirection;
+    }
+}
