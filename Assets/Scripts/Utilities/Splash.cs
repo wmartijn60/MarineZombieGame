130deccf1680f0dc43c72d29c5c1dffb5422d8f3
ImAShark <killersshadowlol@gmail.com>
2021-04-20 15:58:30 +0200

    Can throw water balloon

---
+++ Assets/Scripts/Utilities/Splash.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Splash : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    public void MakeASplash()
+    {
+        Debug.Log("Nothing Happened");
+    }
+
+}
