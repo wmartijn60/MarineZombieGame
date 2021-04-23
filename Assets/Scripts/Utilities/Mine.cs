adc470d910b2f86d75de835895c3b36bd0f5905d
ImAShark <killersshadowlol@gmail.com>
2021-04-23 14:24:03 +0200

    Mine working, no slime yet

---
+++ Assets/Scripts/Utilities/Mine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mine : MonoBehaviour
+{
+    [SerializeField]private float explodeDelay = 1f;
+    [SerializeField]private Animator animator;
+    public List<GameObject> slimePuddles;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Zombie")
+        {
+            animator.SetTrigger("Explode");
+        }
+    }
+
+    void OnDestroy()
+    {
+        int r = Random.Range(0, slimePuddles.Count);
+        Instantiate(slimePuddles[r],transform.position,Quaternion.identity);
+    }
+}
