55ac1af82f57a3cfd8da2c6a220191083f8d00e5
wmartijn60 <34513939+wmartijn60@users.noreply.github.com>
2021-05-14 10:53:07 +0200

    AddedBackgroundDoorCloseAnim

---
+++ Assets/BackgroundManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundManager : MonoBehaviour
+{
+    [SerializeField] Animator backgroundAnimator;
+
+
+    private void Start()
+    {
+        OpenDoor();
+    }
+    public void CloseDoor()
+    {
+        backgroundAnimator.SetTrigger("CloseDoor");
+    }
+
+    public void OpenDoor()
+    {
+        backgroundAnimator.SetTrigger("OpenDoor");
+    }
+}
