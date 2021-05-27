e4018c0c0f92f446d8c5ac9251995d6e4e15071c
ImAShark <killersshadowlol@gamil.com>
2021-05-27 10:34:11 +0200

    added info popups, no animations yet

---
+++ Assets/Scripts/InfoPopup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InfoPopup : MonoBehaviour
+{
+    [SerializeField]private GameObject popup;
+    [SerializeField]private TextMeshProUGUI itemName;
+    [SerializeField]private TextMeshProUGUI itemDescription;
+
+    void Start()
+    {
+        
+    }
+
+    public void PlaceItemName(string newName)
+    {
+        itemName.text = newName;
+    }
+
+    public void PlaceItemDescription(string newDescription)
+    {
+        itemDescription.text = newDescription;
+    }
+
+    public void SwitchPanel()
+    {
+        if (popup.activeSelf)
+        {
+            popup.SetActive(false);
+        }
+        else
+        {
+            popup.SetActive(true);
+        }
+    }
+}
