1f1a363d59c6d0de7d30f1145904f369f2247985
ColinvD <vanDongen.colin@gmail.com>
2021-06-08 11:37:06 +0200

    WebGL build attempt with audio fix attempt

---
+++ Assets/Scripts/AudioStarter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioStarter : MonoBehaviour
+{
+    bool audioResumed = false;
+    
+    void Update()
+    {
+        if (FMODUnity.RuntimeManager.HasBanksLoaded && !audioResumed) {
+            ResumeAudio();
+            audioResumed = true;
+        }
+    }
+
+    public void ResumeAudio() {
+        var result = FMODUnity.RuntimeManager.CoreSystem.mixerSuspend();
+        Debug.Log(result);
+        result = FMODUnity.RuntimeManager.CoreSystem.mixerResume();
+        Debug.Log(result);
+        //audioResumed = true;
+        GetComponent<FMODUnity.StudioEventEmitter>().Stop();
+        GetComponent<FMODUnity.StudioEventEmitter>().Play();
+    }
+}
